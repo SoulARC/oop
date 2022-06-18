ca1d38547dcabce6ac1c855760833642a197a3a7
tolya <tolyan9901@gmail.com>
2022-06-18 17:36:46 +0300

    v 1.0

--- src/main/java/core/basesyntax/MainApp.java
+++ src/main/java/core/basesyntax/MainApp.java
@@ -1,5 +1,17 @@
 package core.basesyntax;
 
+import java.lang.reflect.Array;
+
 public class MainApp {
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        Bulldozer bulldozer = new Bulldozer();
+        Excavator excavator = new Excavator();
+        Machine [] machines = new Machine[]{truck, bulldozer, excavator};
+        for ( Machine mach:machines){
+            mach.doWork();
+            mach.stopWork();
+        }
+    }
 
 }
